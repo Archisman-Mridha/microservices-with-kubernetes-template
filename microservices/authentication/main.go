eb3485f970fc81d99c12b410682bcaee64133daa
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-03 06:36:24 +0530

    implementing start registration rpc in authentication microservice

--- microservices/authentication/main.go
+++ microservices/authentication/main.go
@@ -2,24 +2,47 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"flag"
 	"log"
 	"net"
+	"net/mail"
+	"time"
 
 	"github.com/go-redis/redis"
+	_ "github.com/lib/pq"
 	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/proto"
 
 	protocGenerated "authentication/generated/proto"
+	sqlcGenerated "authentication/generated/sqlc"
 )
 
-const (
+var (
 	OTPQueueName= "otp"
 	ProfileQueueName= "profile"
+
+	ServerError= "server error occured"
+
+	EmailValidationError= "error validating email"
+	NameValidationError= "name should be between 3 to 50 characters"
+
+	SendOTP_RabbitMQMessageType= "SendOTP"
 )
 
-func connectToRabbitMQ( ) func( ) {
+type GlobalVariables struct {
+
+	Repository sqlcGenerated.Querier
+	RedisClient *redis.Client
+	RabbitMQChannel *amqp.Channel
+}
+
+var globalVariables= &GlobalVariables{ }
+
+func connectToRabbitMQ( ) (*amqp.Channel, func( )) {
 	connection, error := amqp.Dial("amqp://user:password@localhost:5672")
 	if error != nil {
 		log.Fatal("💀 error connecting to rabbitMQ : ", error.Error( ))}
@@ -38,19 +61,95 @@ func connectToRabbitMQ( ) func( ) {
 
 	log.Println("🔥 connected to rabbitMQ")
 
-	return func( ) {
+	return channel, func( ) {
 
 		defer channel.Close( )
 		defer connection.Close( )
 	}
 }
 
+func connectToCockroachDB( ) {
+	connection, error := sql.Open("postgres", "postgresql://root@localhost:26257/defaultdb?sslmode=disable")
+	if error != nil {
+		log.Panic("💀 error connecting to cockroachDB : ", error.Error( )) }
+
+	error= connection.Ping( )
+	if error != nil {
+		log.Panic("💀 error pinging cockroachDB : ", error.Error( )) }
+
+	globalVariables.Repository= sqlcGenerated.New(connection)
+
+	log.Println("🔥 connected to cockroachDB")
+}
+
 type ImplementedAuthenticationService struct {
 	*protocGenerated.UnimplementedAuthenticationServer
 }
 
+type TemporaryUserDetails struct {
+	IsVerified bool
+
+	Name string
+	Email string
+}
+
 func(*ImplementedAuthenticationService) StartRegistration(
 	ctx context.Context, request *protocGenerated.StartRegistrationRequest) (*protocGenerated.StartRegistrationResponse, error) {
+
+	//! input validation
+
+	_, error := mail.ParseAddress(request.Email)
+	if error!= nil {
+		return &protocGenerated.StartRegistrationResponse{Error: &EmailValidationError}, nil }
+
+	if len(request.Name) < 3 || len(request.Name) > 50 {
+		return &protocGenerated.StartRegistrationResponse{Error: &NameValidationError}, nil }
+
+	//! saving the details temporarily for 5 minutes in redis
+
+	temporaryUserDetails, error := json.Marshal(
+		&TemporaryUserDetails{
+			IsVerified: false,
+
+			Name: request.Name,
+			Email: request.Email,
+		},
+	)
+
+	if error != nil {
+		log.Println("error : ", error.Error( ))
+		return &protocGenerated.StartRegistrationResponse{Error: &ServerError}, nil
+	}
+
+	error= globalVariables.RedisClient.Set(request.Email, temporaryUserDetails, 600 * time.Second).Err( )
+	if error != nil {
+		log.Println("error : ", error.Error( ))
+		return &protocGenerated.StartRegistrationResponse{Error: &ServerError}, nil
+	}
+
+	//! sending request to the otp service for sending otp to the email address
+
+	message, error := proto.Marshal(
+		&protocGenerated.SendOTPRequest{
+			MessageType: SendOTP_RabbitMQMessageType,
+
+			Email: request.Email,
+		},
+	)
+	if error != nil {
+		log.Println("error : ", error.Error( ))
+		return &protocGenerated.StartRegistrationResponse{Error: &ServerError}, nil
+	}
+
+	error= globalVariables.RabbitMQChannel.Publish(
+		"", OTPQueueName,
+		false, false,
+		amqp.Publishing{ Body: message },
+	)
+	if error != nil {
+		log.Println("error : ", error.Error( ))
+		return &protocGenerated.StartRegistrationResponse{Error: &ServerError}, nil
+	}
 
 	return &protocGenerated.StartRegistrationResponse{ }, nil
 }
@@ -58,8 +157,10 @@ func(*ImplementedAuthenticationService) StartRegistration(
 func main( ) {
 
 	//* connecting to rabbitMQ
-	cleanupRabbitMQResources := connectToRabbitMQ( )
+	rabbitMQChannel, cleanupRabbitMQResources := connectToRabbitMQ( )
 	defer cleanupRabbitMQResources( )
+
+	globalVariables.RabbitMQChannel= rabbitMQChannel
 
 	//* connecting to redis
 
@@ -77,7 +178,11 @@ func main( ) {
 		log.Fatal("💀 error connecting to redis : ", error.Error( )) }
 	defer redisClient.Close( )
 
+	globalVariables.RedisClient= redisClient
 	log.Println("🔥 connected to redis")
+
+	//* connecting to cockroachDB
+	connectToCockroachDB( )
 
 	//* starting the gRPC server
 
