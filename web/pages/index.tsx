6f0ba17b0cfc900fec0187c538c5b16f398a712f
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-04 07:44:27 +0530

    bootstrapping frontend using Nx and NextJS

---
+++ web/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import type { NextPage } from "next"
+
+const HomePage: NextPage= ( ) => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default HomePage
