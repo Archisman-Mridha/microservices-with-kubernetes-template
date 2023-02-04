d83506394e5e5ae4e185ea85d974139f9ddb2ae9
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-04 15:31:39 +0530

    ignore

--- microservices/otp/build/generated/source/proto/main/java/com/otp/generated/proto/VerifyOtpRequestOrBuilder.java
+++ microservices/otp/build/generated/source/proto/main/java/com/otp/generated/proto/VerifyOtpRequestOrBuilder.java
@@ -3,34 +3,31 @@
 
 package com.otp.generated.proto;
 
-public interface VerifyOtpRequestOrBuilder
-        extends
-        // @@protoc_insertion_point(interface_extends:otp.VerifyOtpRequest)
-        com.google.protobuf.MessageOrBuilder {
+public interface VerifyOtpRequestOrBuilder extends
+    // @@protoc_insertion_point(interface_extends:otp.VerifyOtpRequest)
+    com.google.protobuf.MessageOrBuilder {
 
-    /**
-     * <code>string email = 1;</code>
-     *
-     * @return The email.
-     */
-    java.lang.String getEmail();
-    /**
-     * <code>string email = 1;</code>
-     *
-     * @return The bytes for email.
-     */
-    com.google.protobuf.ByteString getEmailBytes();
+  /**
+   * <code>string email = 1;</code>
+   * @return The email.
+   */
+  java.lang.String getEmail();
+  /**
+   * <code>string email = 1;</code>
+   * @return The bytes for email.
+   */
+  com.google.protobuf.ByteString
+      getEmailBytes();
 
-    /**
-     * <code>string otp = 2;</code>
-     *
-     * @return The otp.
-     */
-    java.lang.String getOtp();
-    /**
-     * <code>string otp = 2;</code>
-     *
-     * @return The bytes for otp.
-     */
-    com.google.protobuf.ByteString getOtpBytes();
+  /**
+   * <code>string otp = 2;</code>
+   * @return The otp.
+   */
+  java.lang.String getOtp();
+  /**
+   * <code>string otp = 2;</code>
+   * @return The bytes for otp.
+   */
+  com.google.protobuf.ByteString
+      getOtpBytes();
 }
