5bb5534f0e5d559561ca37a833aae76aab772f3d
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-05 06:06:22 +0530

    creating the simplest gRPC server in kotlin

--- microservices/posts/src/main/kotlin/posts/App.kt
+++ microservices/posts/src/main/kotlin/posts/App.kt
@@ -1,3 +1,15 @@
 package posts
 
-fun main( ) { }
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
+fun main( ) {
+
+    val server: Server= ServerBuilder
+        .forPort(4000)
+        .build( )
+
+    println("🔥 server starting at port 4000")
+    server.start( )
+    server.awaitTermination( )
+}
