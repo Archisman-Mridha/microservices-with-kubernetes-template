0c9b139e68b39da4fc0b6ad5ce1837c57f7efc3b
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-02-05 06:32:31 +0530

    adding reflection to the gRPC server of posts microservice

--- microservices/posts/src/main/kotlin/posts/App.kt
+++ microservices/posts/src/main/kotlin/posts/App.kt
@@ -2,11 +2,13 @@ package posts
 
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import io.grpc.protobuf.services.ProtoReflectionService
 
 fun main( ) {
 
     val server: Server= ServerBuilder
         .forPort(4000)
+        .addService(ProtoReflectionService.newInstance( ))
         .build( )
 
     println("🔥 server starting at port 4000")
